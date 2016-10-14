fd0b9ca1815b21aa25b4cd5d29541cb78ac3326f
adrianaguu <adriana.gutierrez@ucsp.edu.pe>
2016-10-14 13:19:30 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -1,109 +1,161 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char convert (char *c) //ejercicio 1
+struct node
 {
-    if (*c<='Z'&&*c>='A')
-        return *c -'A'+'a';
+    int a;
+    struct node* next;
+
+};
+
+
+struct node * list (int n)
+{
+    struct node* head;
+    head=malloc(sizeof(struct node));
+    struct node* prev;
+    struct node* cur;
+    int i;
+    head->a=1;
+    prev=head;
+    for (i=2;i<=n;i++)
+    {
+        cur=malloc(sizeof(struct node));
+        cur->a=i;
+        prev->next=cur;
+        prev=cur;
+    }
+    prev->next=NULL;
+    return head;
+}
+
+struct node* insertb (int x,struct node* head)
+{
+    struct node* temp;
+    temp=malloc(sizeof(struct node));
+    temp->a=x;
+    temp->next=head;
+    return temp;
+
+}
+
+struct node* inserto (struct node* head,int x)
+{
+    struct node* temp;
+    struct node* temp1;
+    struct node* nuevo;
+    nuevo=malloc(sizeof(struct node));
+    nuevo->a=x;
+    temp = head;
+    do
+    {
+        temp1=temp;
+        temp=temp->next;
+    }while (x>temp->a && temp->next!=NULL);
+    temp->next=nuevo;
+    nuevo->next=NULL;
+    return head;
+}
+
+struct node* insertm (int x,struct node* head)
+{
+    struct node* temp;
+    struct node* temp1;
+    struct node* temp2;
+    temp2=malloc(sizeof(struct node));
+    temp2->a=x;
+    temp=head;
+    int i,n=0;
+    while(temp!=NULL)
+    {
+        temp=temp->next;
+        n++;
+    }
+    temp=head;
+    for (i=0;i<n/2;i++)
+    {
+        temp1=temp;
+        temp=temp->next;
+    }
+    temp1->next=temp2;
+    temp2->next=temp;
+    return head;
+}
+
+void concatenar (struct node* head1, struct node* head2)
+{
+    struct node* temp=head1;
+    while (temp->next!=NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=head2;
+}
+void print (struct node* head)
+{
+    struct node* temp;
+    temp = head;
+    while (temp!=NULL)
+    {
+        printf( "%d \n" , temp->a);
+        temp=temp->next;
+    }
+}
+
+struct node* eliminar (int val,struct node* head)
+{
+    struct node* prev=head;
+    struct node* cur=head->next;
+    if(head->a==val)
+    {
+            cur=head;
+            head=head->next;
+            free(cur);
+            return head;
+    }
     else
-        return *c;
-}
-void strcat(char *s,char *t) 
-{
-    while(*s!='\0')
-        s++;
-
-    while((*s++=*t++)!='\0')
-    ;
-}
-
-int mylen (char *s)
-{
-    int i=0;
-    while (*s!='\0')
     {
-        s++;
-        ++i;
-    }
-    if (*s=='\0')
-        ++i;
-    return i;
-}
-
-int strend(char *s,char *t)
-{
-    int len;
-    len=mylen(t);
-    while(*s!='\0')
-        ++s;
-    while(*t!='\0')
-        ++t;
-    while(len > 0)
-    {
-        if(*t==*s)
+        while (cur!=NULL)
         {
-            --t;
-            --s;
-            --len;
+
+            if (cur->a==val)
+            {
+                prev->next=cur->next;
+                free(cur);
+                break;
+            }
+            prev = cur;
+            cur=cur->next;
+
         }
-        else
-            return 0;
-    }
-    if( len == 0)
-        return 1;
-}
-
-void strncat (char *s, char *t, int n)
-{
-    while (*s!='\0')
-    {
-        s++;
-    }
-    while ( n-->0)
-        *s++=*t++;
-    *s='\0';
-
-}
-
-void strncopy (char*to,char *from,int n)
-{
-
-    while(*from && n-->0)
-    {
-        *to++=*from++;
+        return head;
     }
 
-    *to='\0';
 }
 
-int strncmp (char *s,char *t , int n)
-{
-    for(; *s == *t; s++,t++)
-        if( *s == '\0' || --n <=0)
-            return 0;
-    return *s - *t;
-}
 
 int main()
 {
-    char test[15];
-    char s[15] = {'a','s', 'd','a','r','\0'};
-    char t[15] = {'1','2','3','.','4','5','e','-','6','\0'};
-    strcat(s,t); //concatena t a s
-    printf("%d \n",strend(s,t)); //prueba si t esta al final de s
 
-    char c='B';
-    char *q;
-    q=&c;
-    printf("%c \n",convert(q)); //concvierte mayuscula a minuscula
+    struct node * head =list (7);
+    struct node * head2 =list (4);
+    print(head);
+    printf("\n");
+    head=insertb(0,head);
+    print(head);
+    printf("\n");
+    head=eliminar(0,head);
+    print(head);
+    printf("\n");
+    head=inserto(head,8);
+    print(head);
+    printf("\n");
+    head=insertm(15,head);
+    print(head);
+    printf("\n");
 
-    strncopy(test,t,5); //copia a los 5 primeros elementos de t a test
-    strncat(t,s,3); // copia los 3 primeros elementos de s al final de t
-
-    printf("%d \n",strncmp(s,t,6)); // compara que cadena es mayor
-    printf("%s \n",test); 
-    printf("%s \n",s);
-    printf("%s \n",t);
+    concatenar(head,head2);
+    print(head);
 
 
     return 0;
